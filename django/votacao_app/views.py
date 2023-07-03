8c9b36df573c1ec58df284768b02c1c6d6547638
Pedro Rafante <pedrorafante@gmail.com>
2023-07-02 22:56:38 -0300

    Mudança da logica de verificar email, alteração de css e alteração de mensagem

--- django/votacao_app/views.py
+++ django/votacao_app/views.py
@@ -1,42 +1,34 @@
-from django.shortcuts import render, HttpResponse ,redirect 
+from django.shortcuts import render, HttpResponse, redirect
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
-from .models import Aluno,Grupo,Votacao
+from .models import Aluno, Grupo, Votacao
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseNotFound
 import random
 
-
 def home(request):
-    cookies = request.COOKIES
-    email = request.POST.get('email')
-    reponse = render(request, 'verificar_senha.html')
-    reponse.set_cookie('email', email)
-    emails = Aluno.objects.values_list('email')
-    x = []
-    for mail in emails:
-        x.append(mail[0])
-    msg = x
-    if email in x:
-        num = random.randint(1000, 9999)
-        num = str(num)
-        cod = Aluno.objects.get(email=email)
-        cod.codigo = num
-        cod.save()
-        send_mail('assunto', num, 'votacaoproz@gmail.com', [email, ])
+    if request.method == 'POST':
+        cookies = request.COOKIES
         reponse = render(request, 'verificar_senha.html')
+        email = request.POST.get('email')
         reponse.set_cookie('email', email)
-        return reponse
+        try:
+            aluno = Aluno.objects.get(email=email)
+            num = random.randint(1000, 9999)
+            num = str(num)
+            aluno.codigo = num
+            aluno.save()
+            send_mail('assunto', num, 'votacaoproz@gmail.com', [email, ])
+            reponse = render(request, 'verificar_senha.html')
+            return reponse
+        except Aluno.DoesNotExist:
+            aluno = None
+            reponse = render(request, 'login.html', {'messages': 'email-nao-cadastrado'})
 
-    elif email == None:
-        reponse = render(request, 'login.html', {'messages': None})
-        reponse.set_cookie('email', email)
-        return reponse
+            return reponse
     else:
-        reponse = render(request, 'login.html', {'messages': 'teste'})
-        reponse.set_cookie('email', email)
+        reponse = render(request, 'login.html', {'messages': None})
         return reponse
-
 
 def verificar(request):
     cookies = request.COOKIES
@@ -58,46 +50,46 @@ def verificar(request):
                 user = User.objects.get(username=cod.nome)
                 # user.set_password(cod.codigo)
                 user.save
-                login(request,user)
-                print(user.is_authenticated) 
-                return redirect('votacao')            
-            except:                
+                login(request, user)
+                print(user.is_authenticated)
+                return redirect('votacao')
+            except:
                 print(465)
-                user = User.objects.create_user(cod.nome,cod.email,cod.codigo)
-                login(request,user) 
-            return redirect(request,votacao)
+                user = User.objects.create_user(
+                    cod.nome, cod.email, cod.codigo)
+                login(request, user)
+            return redirect(request, votacao)
         else:
             return render(request, 'verificar_senha.html')
     else:
         reponse = render(request, 'login.html', {'msg': 'teste'})
         reponse.set_cookie('email', cookies['email'])
         return reponse
-    
+
 
 def votacao(request):
     # try:
-        user = request.user
-        grupos = Grupo.objects.values_list('nome_grupo')
-        aluno = Aluno.objects.get(nome = user.username)
-        grupos_tratados = []
-        for i in grupos:
-            grupos_tratados.append(i[0])
-        voto = request.POST.get('grupo')
-        grupos_tratados.remove(aluno.grupo.nome_grupo)
-        print(voto,Votacao.objects.filter(aluno=user.username).exists())
-        if user.is_authenticated:
-            if voto :  
-                if not Votacao.objects.filter(aluno=user.username).exists() :            
-                    votacao =Votacao.objects.create(aluno = user.username,grupo = voto )
-                    voto = False
-                else:
-                    return render(request, 'confirmar-voto.html',{'user':user})
-            if Votacao.objects.filter(aluno=user.username).exists() :
-                return render(request, 'confirmar-voto.html',{'user':user})
-            return render(request, 'tela_votacao.html',{'grupos':grupos_tratados})
-        else:
-            return redirect(verificar,{'messages': 'teste'})
+    user = request.user
+    grupos = Grupo.objects.values_list('nome_grupo')
+    aluno = Aluno.objects.get(nome=user.username)
+    grupos_tratados = []
+    for i in grupos:
+        grupos_tratados.append(i[0])
+    voto = request.POST.get('grupo')
+    grupos_tratados.remove(aluno.grupo.nome_grupo)
+    print(voto, Votacao.objects.filter(aluno=user.username).exists())
+    if user.is_authenticated:
+        if voto:
+            if not Votacao.objects.filter(aluno=user.username).exists():
+                votacao = Votacao.objects.create(
+                    aluno=user.username, grupo=voto)
+                voto = False
+            else:
+                return render(request, 'confirmar-voto.html', {'user': user})
+        if Votacao.objects.filter(aluno=user.username).exists():
+            return render(request, 'confirmar-voto.html', {'user': user})
+        return render(request, 'tela_votacao.html', {'grupos': grupos_tratados})
+    else:
+        return redirect(verificar, {'messages': 'teste'})
     # except:
     #     return HttpResponseNotFound('ola')
-        
-    
