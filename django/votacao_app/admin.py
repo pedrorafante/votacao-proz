bc4ef29a08d189dea817624a56f0b0ea605a81d2
Douglas <dhrds1996@gmail.com>
2023-07-05 11:59:15 -0300

    add votacao no admin

--- django/votacao_app/admin.py
+++ django/votacao_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Aluno,Grupo
+from .models import Aluno,Grupo ,Votacao
 
 # Register your models here.
 admin.site.register(Aluno)
 admin.site.register(Grupo)
+admin.site.register(Votacao)
